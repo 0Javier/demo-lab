3a65c41868e9955229f887a796d833ed059e5e67
Javier Chamorro <javierk38@hotmail.com>
2024-06-20 11:44:16 -0500

    agrego url de prueba

--- app/example/settings.py
+++ app/example/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ("localhost", "127.0.0.1", "demo-lab.local"))
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ("localhost", "127.0.0.1", "demo-lab.local", "demo2-lab.local"))
 
 
 # Application definition
